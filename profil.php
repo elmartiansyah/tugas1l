203545f6dff626587a37f1ae5995a9a01c173404
surya sentana <surya.sentana@student.unsil.ac.id>
2014-05-01 14:56:28 +0700

    first One

---
+++ profil.php
@@ -0,0 +1,39 @@
+<?php session_start(); ?>
+  <?php include("header.php"); ?>
+</tr>
+  <tr>
+    <td width="171" height="332" valign="top" bordercolor="#000000" background="images/bg1.JPG">
+	<?php include("nav.php"); ?>
+    <td width="581" valign="top" border="2" background="images/bg3.JPG" bgcolor="#FF0066">
+	<br/>
+	<p align="center" style="font-weight: bold">Halaman Profil</p>
+	<br/>
+	<table align="center" width="379" border="0">
+      <tr>
+        <td width="51">Nama</td>
+        <td width="3">:</td>
+        <td width="222">Surya Sentana </td>
+      </tr>
+      <tr>
+        <td>TTL</td>
+        <td>:</td>
+        <td>Majalengka, 18 Mei 1992 </td>
+      </tr>
+      <tr>
+        <td>Alamat</td>
+        <td>:</td>
+        <td>Jalan Imbangkencana No. 15 </td>
+      </tr>
+      <tr>
+        <td>Website</td>
+        <td>:</td>
+        <td><a href="http://github.com/Surya18/Tugas1L">http://github.com/Surya18/Tugas1L</a></td>
+      </tr>
+      <tr>
+        <td>Hobby</td>
+        <td>:</td>
+        <td>Makan :) </td>
+      </tr>
+    </table></td>
+  </tr>
+<?php include("footer.php"); ?>
